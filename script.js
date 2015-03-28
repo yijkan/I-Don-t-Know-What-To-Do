813b1a95cc7da0456c73992e7a1c39a9d3fa1089
tchow5 <timchow26@yahoo.com>
2015-03-28 18:55:34 -0400

    added index generator

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
+var randomideas = [ '<a href = "http://www.tropicalmba.com/learn-to-play-guitar/">learn how to play guitar</a>', 
+ '<a href = "http://www.auntiepixelante.com/twine/">learn how to make interactive stories with the free program Twine</a>', 
+ '<a href = "https://www.duolingo.com/">learn another language</a>' ];
+
+
+
+
 $(document).ready(function() {
 	//$("#suggestion")
 
 	$("#random").click(function() {
+		var randnum = Math.floor((Math.random()* randomideas.length));
+		var randomidea = randomideas[randnum];
+		document.getElementById("#suggestion").innerHTML = randomidea;
 		
 	})
 })

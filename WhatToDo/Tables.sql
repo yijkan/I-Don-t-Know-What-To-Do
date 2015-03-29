e881e8b2ccd31623489e94f2329d13c74a54d9a6
Jinnie <yijkan@bergen.org>
2015-03-29 02:03:43 -0400

    Added more suggestions

--- WhatToDo/Tables.sql
+++ WhatToDo/Tables.sql
@@ -10,6 +10,21 @@ CREATE TABLE IF NOT EXISTS `Suggestions` (
 );
 
 INSERT INTO Suggestions (Suggestion, Category)
+VALUES ('Make senpai notice you', 0);
+
+INSERT INTO Suggestions (Suggestion, Category)
+VALUES ('Do your homework', 0);
+
+INSERT INTO Suggestions (Suggestion, Category)
+VALUES ('Make a blanket fort', 0);
+
+INSERT INTO Suggestions (Suggestion, Category)
+VALUES ('<a href=”http://www.tasteofhome.com/recipes/holiday---celebration-recipes/christmas-recipes/how-to-make-candy”>Make candy</a>', 1);
+
+INSERT INTO Suggestions (Suggestion, Category)
+VALUES ('<a href=”http://www.goinghometoroost.com/2011/diy-craft-projects/how-to-make-your-own-tea/”>Make your own tea</a>', 1);
+
+INSERT INTO Suggestions (Suggestion, Category)
 VALUES ("<a href = 'http://www.tropicalmba.com/learn-to-play-guitar/'>Learn how to play guitar</a>", 2);
 
 INSERT INTO Suggestions (Suggestion, Category)
@@ -26,3 +41,6 @@ VALUES ('<a href=”http://sublimestitching.com/pages/tutorials”>Learn how to 
 
 INSERT INTO Suggestions (Suggestion, Category)
 VALUES ('<a href=”http://howset.com/cat/how-to-make-origami/#.VRc15PnF-Cl”>Learn how to do origami</a>', 2);
+
+INSERT INTO Suggestions (Suggestion, Category)
+VALUES ('<a href=”http://magic.about.com/od/libraryofsimpletricks/ig/Easy-Magic-Tricks/”>Learn how to do magic</a>', 2);

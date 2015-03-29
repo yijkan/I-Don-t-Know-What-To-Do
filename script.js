acd54a19cd2abcdc8852f207aafa5dfbaee39a80
tchow5 <timchow26@yahoo.com>
2015-03-28 20:23:22 -0400

    random generator ideas work

--- script.js
+++ script.js
@@ -2,9 +2,15 @@ var randomideas = [ '<a href = "http://www.tropicalmba.com/learn-to-play-guitar/
  '<a href = "http://www.auntiepixelante.com/twine/">learn how to make interactive stories with the free program Twine</a>', 
  '<a href = "https://www.duolingo.com/">learn another language</a>' ];
 
-var learn = [];
-var food = [];
-var misc = [];
+var learn = ['<a href = "http://www.tropicalmba.com/learn-to-play-guitar/">learn how to play guitar</a>', 
+ '<a href = "http://www.auntiepixelante.com/twine/">learn how to make interactive stories with the free program Twine</a>', 
+ '<a href = "https://www.duolingo.com/">learn another language</a>'];
+var food = ['<a href = "http://www.tropicalmba.com/learn-to-play-guitar/">learn how to play guitar</a>', 
+ '<a href = "http://www.auntiepixelante.com/twine/">learn how to make interactive stories with the free program Twine</a>', 
+ '<a href = "https://www.duolingo.com/">learn another language</a>'];
+var misc = ['<a href = "http://www.tropicalmba.com/learn-to-play-guitar/">learn how to play guitar</a>', 
+ '<a href = "http://www.auntiepixelante.com/twine/">learn how to make interactive stories with the free program Twine</a>', 
+ '<a href = "https://www.duolingo.com/">learn another language</a>'];
  
 var random = learn.concat(food);
 random = random.concat(misc);
@@ -14,26 +20,26 @@ $(document).ready(function() {
 
 	$("#random").click(function() {
 		var randnum = Math.floor((Math.random()* randomideas.length));
-		var randomidea = random[randnum];
+		var randomidea = randomideas[randnum];
 		document.getElementById("suggestion").innerHTML = randomidea;
 		
 	})
 	
-	$("#random").click(function() {
+	$("#learn").click(function() {
 		var randnum = Math.floor((Math.random()* learn.length));
 		var randomidea = learn[randnum];
 		document.getElementById("suggestion").innerHTML = randomidea;
 		
 	})
 	
-	$("#random").click(function() {
+	$("#food").click(function() {
 		var randnum = Math.floor((Math.random()* food.length));
 		var randomidea = food[randnum];
 		document.getElementById("suggestion").innerHTML = randomidea;
 		
 	})
 	
-	$("#random").click(function() {
+	$("#misc").click(function() {
 		var randnum = Math.floor((Math.random()* misc.length));
 		var randomidea = misc[randnum];
 		document.getElementById("suggestion").innerHTML = randomidea;

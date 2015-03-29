67e2df5a863b805c4887d321aade98dc6c6a5506
Jinnie <yijkan@bergen.org>
2015-03-28 21:02:15 -0400

    cleaned up format

--- script.js
+++ script.js
@@ -2,15 +2,21 @@ var randomideas = [ '<a href = "http://www.tropicalmba.com/learn-to-play-guitar/
  '<a href = "http://www.auntiepixelante.com/twine/">learn how to make interactive stories with the free program Twine</a>', 
  '<a href = "https://www.duolingo.com/">learn another language</a>' ];
 
-var learn = ['<a href = "http://www.tropicalmba.com/learn-to-play-guitar/">learn how to play guitar</a>', 
- '<a href = "http://www.auntiepixelante.com/twine/">learn how to make interactive stories with the free program Twine</a>', 
- '<a href = "https://www.duolingo.com/">learn another language</a>'];
-var food = ['<a href = "http://www.tropicalmba.com/learn-to-play-guitar/">learn how to play guitar</a>', 
- '<a href = "http://www.auntiepixelante.com/twine/">learn how to make interactive stories with the free program Twine</a>', 
- '<a href = "https://www.duolingo.com/">learn another language</a>'];
-var misc = ['<a href = "http://www.tropicalmba.com/learn-to-play-guitar/">learn how to play guitar</a>', 
- '<a href = "http://www.auntiepixelante.com/twine/">learn how to make interactive stories with the free program Twine</a>', 
- '<a href = "https://www.duolingo.com/">learn another language</a>'];
+var learn = [
+	'<a href = "http://www.tropicalmba.com/learn-to-play-guitar/">learn how to play guitar</a>',
+	'<a href = "http://www.auntiepixelante.com/twine/">learn how to make interactive stories with the free program Twine</a>', 
+	'<a href = "https://www.duolingo.com/">learn another language</a>'
+];
+var food = [
+	'<a href = "http://www.tropicalmba.com/learn-to-play-guitar/">learn how to play guitar</a>', 
+	'<a href = "http://www.auntiepixelante.com/twine/">learn how to make interactive stories with the free program Twine</a>', 
+	'<a href = "https://www.duolingo.com/">learn another language</a>'
+];
+var misc = [
+	'<a href = "http://www.tropicalmba.com/learn-to-play-guitar/">learn how to play guitar</a>', 
+	'<a href = "http://www.auntiepixelante.com/twine/">learn how to make interactive stories with the free program Twine</a>', 
+	'<a href = "https://www.duolingo.com/">learn another language</a>'
+];
  
 var random = learn.concat(food);
 random = random.concat(misc);
@@ -19,31 +25,31 @@ $(document).ready(function() {
 	//$("#suggestion")
 
 	$("#random").click(function() {
-		var randnum = Math.floor((Math.random()* randomideas.length));
-		var randomidea = randomideas[randnum];
+		var randnum = Math.floor(Math.random()* random.length);
+		var randomidea = random[randnum];
 		document.getElementById("suggestion").innerHTML = randomidea;
-		
+		document.getElementById("directions").innerHTML = "Click again to get another suggestion";
 	})
 	
 	$("#learn").click(function() {
-		var randnum = Math.floor((Math.random()* learn.length));
+		var randnum = Math.floor(Math.random()* learn.length);
 		var randomidea = learn[randnum];
 		document.getElementById("suggestion").innerHTML = randomidea;
-		
+		document.getElementById("directions").innerHTML = "Click again to get another suggestion";	
 	})
 	
 	$("#food").click(function() {
-		var randnum = Math.floor((Math.random()* food.length));
+		var randnum = Math.floor(Math.random()* food.length);
 		var randomidea = food[randnum];
 		document.getElementById("suggestion").innerHTML = randomidea;
-		
+		document.getElementById("directions").innerHTML = "Click again to get another suggestion";
 	})
 	
 	$("#misc").click(function() {
-		var randnum = Math.floor((Math.random()* misc.length));
+		var randnum = Math.floor(Math.random()* misc.length);
 		var randomidea = misc[randnum];
 		document.getElementById("suggestion").innerHTML = randomidea;
-		
+		document.getElementById("directions").innerHTML = "Click again to get another suggestion";
 	})
 
 
